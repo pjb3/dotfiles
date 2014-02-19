3757fb66d5db0b0cb623c2fa027dcb4ef31a4d11
Paul Barry <mail@paulbarry.com>
2014-02-18 20:46:49 -0500

    Added some git settings
    Some of these are based on the Git Immersion tutorial

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ for file in `ls -a | egrep '^\.\w'`; do
     ln -fns dotfiles/$file ~/$file
   fi
 done
+
+# Configure Sublime
+cd /Users/paul/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+rm Preferences.sublime-settings
+ln -s ~/dotfiles/Preferences.sublime-settings
+
+# Install rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
